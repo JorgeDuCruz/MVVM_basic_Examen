30859c307714f6a8375a86aa3c083b00e116db94
jorge <jdurancruz@danielcastelao.org>
2025-11-21 10:10:33 +0100

    Ej 3 hecho

--- app/src/main/java/com/dam/mvvm_basic/Datos.kt
+++ app/src/main/java/com/dam/mvvm_basic/Datos.kt
@@ -41,8 +41,8 @@ enum class Estados(val start_activo: Boolean, val boton_activo: Boolean) {
  * Estados auxiliares para corutinas en el ViewModel
  * @param txt: String nombre del estado
  */
-enum class EstadosAuxiliares(val txt: String) {
-    AUX1(txt = "aux1"),
-    AUX2(txt = "aux2"),
-    AUX3(txt = "aux3"),
+enum class EstadosAuxiliares(val txt: String, val funcion: (String)-> String) {
+    AUX1(txt = "aux1", funcion = {msj -> msj}),
+    AUX2(txt = "aux2", funcion = {msj -> msj.lowercase()}),
+    AUX3(txt = "aux3", funcion = {msj -> msj.uppercase()}),
 }
